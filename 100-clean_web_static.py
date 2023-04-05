668978122bae451de54c6e7c841c170057208864
ZahraJkhan <zahrajid@gmail.com>
2023-04-05 07:28:19 -0700

    Fabric script (based on the file 3-deploy_web_static.py) that deletes out-of-date archives, using the function do_clean

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """A module for web application deployment with Fabric."""
-import os
+import os.path
 from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
